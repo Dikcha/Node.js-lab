e9ee829ecb8e81e3251f60b2eb46b2f781c5fa56
Dikcha <budaev.1999@mail.ru>
2019-04-01 19:24:57 -0800

    add logger

---
+++ src/app/services/logger/index.ts
@@ -0,0 +1,35 @@
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+
+
+export class LoggerService {
+    private readonly logger;
+
+    constructor() {
+
+        this.logger = expressWinston.logger({
+            transports: [
+                new winston.transports.File({
+                    level: 'info',
+                    filename: './log.log',
+                    maxsize: 10000,
+                }),
+            ],
+            format: winston.format.combine(
+                winston.format.timestamp({
+                    format: 'YYYY-MM-DD HH:mm:ss'
+                }),
+                winston.format.simple()
+            ),
+            msg: `{{res.statusCode}} {{req.method}} {{req.url}} {{JSON.stringify(req.body)}} {{JSON.stringify(req.query)}} {{res.responseTime}}`,
+            meta: false,
+            expressFormat: false,
+            colorize: false,
+            exitOnError: false,
+        });
+    }
+
+    getLogger() {
+        return this.logger;
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Routes } from "./app/routes";
 import  { db as sequelize } from './database/sequelize';
+import { LoggerService } from "./app/services/logger";
 
 async function initializeServer() {
     const server = express();
     server.use(bodyParser.json());
+    const loggerService = new LoggerService();
+    const logger = loggerService.getLogger();
+    server.use(logger);
     server.use('/api', Routes);
     await sequelize.sync();
     server.listen(3000, () => {
